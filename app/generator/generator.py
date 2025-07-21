035844c1e89a66a8435a9dbef8cc0ed3e2f2019c
Rayan Benaissa <rayanbenaissa@gmail.com>
2025-07-21 18:58:26 +0200

    adaptation of the generator

--- app/generator/generator.py
+++ app/generator/generator.py
@@ -1,18 +1,27 @@
 from utils.prompts import Prompt
 from utils.models import Models
-from tools.file_manager import PdfManager
+from tools.file_manager import *
+from schema.letter_schema import CoverLetterSchema
 
 class Generator:
-    def __init__(self):
-        self.model = Models.MISTRAL
+    def __init__(self, urls: list[str]):
+        self.model = Models.MISTRAL.with_structured_output(CoverLetterSchema)
         self.prompt = Prompt.GENERATE_MOTIVATION
         self.cv = PdfManager().run()
+        self.urls = urls
 
-    def run(self, applications):
-        chain = self.prompt | self.model.with_structured_output()
+    def run(self):
+        chain = self.prompt | self.model
+        letter_manager = CoverLetterManager()
+        application_manager = ApplicationManager(self.urls)
+        applications = application_manager.run()
+        results = []
         for application in applications:
-            letter = chain.invoke({
-            "cv": self.cv,
-            "job_description": application
-            }).content
+            letter : CoverLetterSchema = chain.invoke({
+                "cv": self.cv,
+                "job_description": application
+            })
+            letter_manager.manage(letter.title, letter.content)
+            results.append(letter)
+        return results
         
