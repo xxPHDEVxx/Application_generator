5fa4b8b7e0c01118224e93d075325264b4f37581
Rayan Benaissa <rayanbenaissa@gmail.com>
2025-07-21 20:07:26 +0200

    add langsmith + correction prompt

--- app/utils/prompts.py
+++ app/utils/prompts.py
@@ -5,14 +5,8 @@ from langchain.prompts import PromptTemplate
 class Prompt:
     GENERATE_MOTIVATION = PromptTemplate.from_template(
         """
-        You are an expert in writing cover letters. Based on the content of this cv : {cv}  and this job description {job_description}, write a professional, well-structured cover letter tailored to the position. The letter must be written in the language of the job description. Do not include any explanations, disclaimers, or additional commentary.
+        You are an assistant that generates structured cover letters.
 
-        Format your response strictly as follows:
-
-        Title: [Title of the letter – e.g., Application for the position of Software Developer]
-
-        Text:
-        [Full cover letter text – including a compelling introduction, a structured body highlighting skills, experience, and motivation, and a polite closing.]
-
+        Based on the provided CV : {cv} and job description {job_description}, return a CoverLetterSchema
         """
     )
