84cbf32338a14b6b1ddaf5e39beb133262c66efc
Rayan Benaissa <rayanbenaissa@gmail.com>
2025-07-21 15:43:56 +0200

    improve prompt + add file manager into generator class

--- app/generator/generator.py
+++ app/generator/generator.py
@@ -1,15 +1,17 @@
 from utils.prompts import Prompt
 from utils.models import Models
-
+from tools.file_manager import PdfManager
 
 class Generator:
     def __init__(self, application: str):
         self.model = Models.MISTRAL
         self.prompt = Prompt.GENERATE_MOTIVATION
         self.application = application
+        self.cv = PdfManager().run()
 
     def run(self):
-        chain = self.model | self.prompt
-        return chain.invoke(
-            self.application
-            )
+        chain = self.prompt | self.model
+        return chain.invoke({
+            "cv": self.cv,
+            "job_description": self.application
+    }).content
--- app/utils/prompts.py
+++ app/utils/prompts.py
@@ -5,6 +5,14 @@ from langchain.prompts import PromptTemplate
 class Prompt:
     GENERATE_MOTIVATION = PromptTemplate.from_template(
         """
-        You generate motivation letter with those informations : {informations}.  
+        You are an expert in writing cover letters. Based on the content of this cv : {cv}  and this job description {job_description}, write a professional, well-structured cover letter tailored to the position. The letter must be written in the language of the job description. Do not include any explanations, disclaimers, or additional commentary.
+
+        Format your response strictly as follows:
+
+        Title: [Title of the letter – e.g., Application for the position of Software Developer]
+
+        Text:
+        [Full cover letter text – including a compelling introduction, a structured body highlighting skills, experience, and motivation, and a polite closing.]
+
         """
     )
